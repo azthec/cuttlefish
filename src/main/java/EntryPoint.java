6e6012b927f00f86c0cddec7786f253567222fca
DBogas <dbogas738@gmail.com>
2018-11-08 17:48:37 +0000

    implemtenting basic versions of cmds

--- src/main/java/EntryPoint.java
+++ src/main/java/EntryPoint.java
@@ -3,23 +3,62 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.nio.file.Paths;
 
 @Path("/api")
 public class EntryPoint {
+
+    //static variables to sim a filesystem
+    java.nio.file.Path basePath = Paths.get(System.getProperty("user.dir"));
+    java.nio.file.Path currentPath = Paths.get(System.getProperty("user.dir"));
+
+
 
     @GET
     @Path("/{param}")
     @Produces(MediaType.TEXT_PLAIN)
     public String process(@PathParam("param") String cmd) {
-        //String cmdRes = exectuteCmd(cmd);
-        System.out.println(cmd);
-        return "You told me to run "+cmd;
+        return exectuteCmd(cmd);
     }
 
     private String exectuteCmd(String cmd){
+        String[] cmd_parted = cmd.split(" ");
+        String res="";
+        switch (cmd_parted[0]){
+            case "ls":
+                res += ls();
+                break;
+            case "pwd":
+                res += pwd();
+                break;
+        }
+        return res;
+    }
+
+    private String ls(){
         return "";
     }
 
+    private String pwd(){
+        return resolvePath(basePath,currentPath).toString();
+    }
+
+    //https://stackoverflow.com/questions/33083397/filtering-upwards-path-traversal-in-java-or-scala
+    public java.nio.file.Path resolvePath(final java.nio.file.Path baseDirPath, final java.nio.file.Path userPath) {
+
+        if (!baseDirPath.isAbsolute())
+            throw new IllegalArgumentException("Base path must be absolute");
+
+        if (userPath.isAbsolute())
+            throw new IllegalArgumentException("User path must be relative");
+
+        final java.nio.file.Path resolvedPath = baseDirPath.resolve(userPath).normalize();
+
+        if (!resolvedPath.startsWith(baseDirPath))
+            throw new IllegalArgumentException("User path escapes the base path");
+
+        return resolvedPath;
+    }
     /*@GET
     @Path("ping")
     @Produces(MediaType.TEXT_PLAIN)
