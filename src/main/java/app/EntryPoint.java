d2428fa138cc7f4fa90e877e8097c031235c25b5
DBogas <dbogas738@gmail.com>
2018-11-12 16:50:42 +0000

    updated docs

--- src/main/java/app/EntryPoint.java
+++ src/main/java/app/EntryPoint.java
@@ -1,20 +1,17 @@
 package app;
 
-import org.glassfish.jersey.jaxb.internal.XmlJaxbElementProvider;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.io.*;
-import java.nio.file.Paths;
 
 @Path("/api")
 public class EntryPoint {
 
-    File baseDir = new File(System.getProperty("user.dir"));
-    File currDir = new File(System.getProperty("user.dir"));
+    private File baseDir = new File(System.getProperty("user.dir"));
+    private File currDir = new File(System.getProperty("user.dir"));
 
 
     @GET
@@ -24,6 +21,18 @@ public class EntryPoint {
         return exectuteCmd(cmd);
     }
 
+    /**
+     * This method executes the commands requested.
+     * Allways test the commands inside the "testfolder" directory, so you don't erase important files by mistake.
+     * @param cmd is the command to execute, passed as a string
+     *            The string is then split.
+     *            Following that we analyse the first word in the command, as most cases benefit from this.
+     *            If the first word is recognized as an implemented function, it is run, taking in consideration
+     *            the remaining parameters.
+     *            If that fails, we enter a default case, where commands such as "<" and ">" can be detected,
+     *            as well as bad/unimplemmented commands.
+     * @return the result of that command, in String form.
+     */
     private String exectuteCmd(String cmd){
         String[] cmd_parted = cmd.split(" ");
         String res="";
@@ -64,10 +73,20 @@ public class EntryPoint {
         return res;
     }
 
+    /**
+     * This is a test function, for debugging purposes.
+     * @return
+     */
     private String test(){
         return baseDir.getName();
     }
 
+    /**
+     * Implementation of mkdir.
+     * Creates a folder in the given path.
+     * @param newFolderPath the path to create the folder in
+     * @return an empty String for success or an error message.
+     */
     private String mkdir(String newFolderPath){
         String res = "";
         File newFolder = new File(newFolderPath);
@@ -92,6 +111,12 @@ public class EntryPoint {
         return res;
     }
 
+    /**
+     * Implementation of rmdir.
+     * Removes a folder if and only if it is empty.
+     * @param folderPath, the folder to remove
+     * @return empty String for success, or an error message.
+     */
     private String rmdir(String folderPath){
         String res = "";
         File target = new File(folderPath);
@@ -111,6 +136,10 @@ public class EntryPoint {
         return res;
     }
 
+    /**
+     * Implementation of the ls command
+     * @return the simplest version of ls (argumentless)
+     */
     private String ls(){
         String res = "";
         for(File file:baseDir.listFiles()){
@@ -123,10 +152,19 @@ public class EntryPoint {
         return res;
     }
 
+    /**
+     * Implementation of pwd command
+     * @return the present work directory
+     */
     private String pwd(){
         return currDir.getName();
     }
 
+    /**
+     * Implementation of cat command
+     * @param filePath, the path to the file we want to cat.
+     * @return
+     */
     private String cat (String filePath){
         String res = "Content: \n";
         File targetFile = new File(filePath);
